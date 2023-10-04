5b29f33b159fae1d827d07e12d7a1119336439bc
Prashant Pathak <113759657+Prashant-pathak-01@users.noreply.github.com>
2023-10-04 09:00:55 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Roman Number to Integer/roman-number-to-integer.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String roman = br.readLine().trim();
+            Solution ob = new Solution();
+            System.out.println(ob.romanToDecimal(roman));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Finds decimal value of a given roman numeral
+    public int romanToDecimal(String str) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        
+        int res = 0;
+        int min = map.get(str.charAt(str.length()-1));
+        for(int i=str.length()-1; i>=0; i--){
+            int temp = map.get(str.charAt(i));
+            if(temp>=min){
+                res+=temp;
+                min = temp;
+            } else{
+                res-=temp;
+            }
+        }
+        return res;
+        
+    }
+}
